ae4ed045ffb3013806773b0818845e67257b4a27
joshuanianji <joshua.ji@jiwhiz.com>
2021-01-28 09:36:23 -0700

    Update to TS and async/await No type errors, but might not compile? Maybe we should test it

--- sma-functions/functions/src/handlers/jokes.ts
+++ sma-functions/functions/src/handlers/jokes.ts
@@ -1,9 +1,29 @@
 /* eslint-disable eqeqeq */
 // Things that have to do with handling jokes. Atm you can make a joke, and get all the jokes.
 import { db } from './../util/admin';
-import { Joke, JokeNoID } from './../types';
+import { Joke, JokeNoID, Comment, instanceOfError, error, JokeWithComments } from './../types';
 
-const getAllJokes = (request: any, response: any) => {
+// EXPRESS ALLOWS ASYNC HANDLERS PGOPOGPOGPOGPO
+// ⠄⠄⠄⠄⠄⠄⣀⣀⣀⣤⣶⣿⣿⣶⣶⣶⣤⣄⣠⣴⣶⣿⣿⣿⣿⣶⣦⣄⠄⠄
+// ⠄⠄⣠⣴⣾⣿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦
+// ⢠⠾⣋⣭⣄⡀⠄⠄⠈⠙⠻⣿⣿⡿⠛⠋⠉⠉⠉⠙⠛⠿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⡎⣾⡟⢻⣿⣷⠄⠄⠄⠄⠄⡼⣡⣾⣿⣿⣦⠄⠄⠄⠄⠄⠈⠛⢿⣿⣿⣿⣿⣿
+// ⡇⢿⣷⣾⣿⠟⠄⠄⠄⠄⢰⠁⣿⣇⣸⣿⣿⠄⠄⠄⠄⠄⠄⠄⣠⣼⣿⣿⣿⣿
+// ⢸⣦⣭⣭⣄⣤⣤⣤⣴⣶⣿⣧⡘⠻⠛⠛⠁⠄⠄⠄⠄⣀⣴⣿⣿⣿⣿⣿⣿⣿
+// ⠄⢉⣹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣦⣶⣶⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⢰⡿⠛⠛⠛⠛⠻⠿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⠸⡇⠄⠄⢀⣀⣀⠄⠄⠄⠄⠄⠉⠉⠛⠛⠻⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⠄⠈⣆⠄⠄⢿⣿⣿⣿⣷⣶⣶⣤⣤⣀⣀⡀⠄⠄⠉⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⠄⠄⣿⡀⠄⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠂⠄⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⠄⠄⣿⡇⠄⠄⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠄⢀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⠄⠄⣿⡇⠄⠠⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠄⠄⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⠄⠄⣿⠁⠄⠐⠛⠛⠛⠛⠉⠉⠉⠉⠄⠄⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿
+// ⠄⠄⠻⣦⣀⣀⣀⣀⣀⣀⣤⣤⣤⣤⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠄
+
+
+
+// change to async await?
+const getAllJokes = (req: any, res: any) => {
     // https://firebase.google.com/docs/firestore/query-data/get-data for helpful documentation
     db.collection('Jokes')
         .orderBy('timeCreated', 'desc') // descending order; i think .get is basically a request (oh its the method?)
@@ -18,11 +38,11 @@ const getAllJokes = (request: any, response: any) => {
                     jokeId: doc.id,
                 });
             });
-            return response.json(jokes); // I guess the response parameter is just the thing we have to edit?
+            return res.json(jokes); // I guess the response parameter is just the thing we have to edit?
         })
         .catch(err => {
             console.error(err);
-            return response.status(500).json({ error: err.code });
+            return res.status(500).json({ error: err.code });
         });
 };
 
@@ -50,45 +70,37 @@ const postOneJoke = (req: any, res: any) => {
         })
 };
 
-const getJokeData = (req: any, res: any) => {
-    // gets all the data aabout the joke, and then also the data w.r.t to the comments
-    let jokeData;
-    const jokeId = req.params.jokeId.trim(); // quick rename
-    db.doc(`Jokes/${jokeId}`).get()
-        .then(doc => {
-            if (doc.exists) {
-                jokeData = doc.data();
-                jokeData.jokeId = doc.id;
-                return;
-            } else {
-                return res.status(404).json({ error: `Joke with id ${jokeId} not found` })
-            }
-        })
-        .catch(err => {
-            console.error(err);
-            return res.status(500).json({ error: err.code });
-        });
 
-    db.collection('comments')
-        .orderBy('timeCreated', 'desc')
-        .where("jokeId", "==", jokeId)
-        .get()
-        .then(data => {
-            jokeData.comments = [];
-            data.forEach(doc => {
-                jokeData.comments.push(doc.data());
-            });
-            return res.status(200).json(jokeData)
-        })
-        .catch(err => {
-            console.error(err);
-            return res.status(400).json({ error: err.code });
-        });
+const getJokeData = async (req: any, res: any) => {
+    // gets all the data about the joke, and then also the data w.r.t to the comments
+
+    try {
+        const jokeId = req.params.jokeId.trim(); // quick rename
+        const jokeData: Joke = await getJoke(jokeId);
+        const comments: Comment[] = await getJokeComments(jokeId);
+        // "return" joke type
+        const retJoke: JokeWithComments = {
+            comments: comments,
+            ...jokeData
+        }
+        return res.status(200).json(retJoke)
+    } catch (err) {
+        // inspired from this article](https://medium.com/@arthurxavier/error-handling-in-long-promise-chains-155f610b5bc6)
+        // except instanceof with interfaces are a bit different
+        if (instanceOfError(err)) {
+            // we have access to status and msg
+            return res.status(err.status).json({ error: err.msg, extraInfo: err.extra })
+        } else {
+            // random msg
+            return res.status(500).json({ msg: `Unknown error: ${err.code}` })
+        }
+    }
 };
 
-const commentOnJoke = (req: any, res: any) => {
+const commentOnJoke = async (req: any, res: any) => {
     if (req.body.body.trim() === '') return res.status(400).json({ body: "Must not be empty" });
-    const newComment = {
+
+    const newComment: Comment = {
         jokeId: req.params.jokeId.trim(),
         timeCreated: new Date().toISOString(),
         userHandle: req.user.handle,
@@ -96,148 +108,107 @@ const commentOnJoke = (req: any, res: any) => {
         body: req.body.body,
     };
 
-    db.doc(`Jokes/${req.params.jokeId}`).get()
-        .then((doc) => {
-            if (doc.exists) {
-                return doc.ref.update({ commentCount: doc.data().commentCount + 1 }); // .ref is a new function woah
-            } else {
-                return res.status(400).json({ error: "joke not found" });
-            }
-        })
-        .then(() => {
-            return db.collection('comments').add(newComment);
-        })
-        .then(doc => {
-            return res.status(200).json(newComment);
-        })
-        .catch(err => {
-            console.error(err);
-            return res.json({ error: err.code });
-        });
+    try {
+        const doc = await db.doc(`Jokes/${req.params.jokeId}`).get();
+
+        if (doc.exists) {
+            const data: Joke = doc.data() as Joke;
+            await doc.ref.update({ commentCount: data.commentCount + 1 }); // .ref is a new function woah
+        } else {
+            return res.status(400).json({ error: "joke not found" });
+        }
+
+        await db.collection('comments').add(newComment);
+        return res.status(200).json(newComment);
+    }
+    catch (err) {
+        console.error(err);
+        return res.status(500).json({ error: err.code });
+    }
 };
 
-const likeJoke = (req: any, res: any) => {
-    const likeDocument = db
-        .collection('likes')
-        .where('userHandle', '==', req.user.handle)
-        .where('jokeId', '==', req.params.jokeId)
-        .limit(1);
+const likeJoke = async (req: any, res: any) => {
+    try {
+        // initialize documents
+        // this fetches firestore twice. Maybe change?
+        const jokeDocument = db.doc(`/Jokes/${req.params.jokeId}`);
+        const jokeData: Joke = await getJoke(req.params.jokeId);
+        const likeDoc = await getLikeDocument(req.user.handle, req.params.jokeId);
 
-    const jokeDocument = db.doc(`/Jokes/${req.params.jokeId}`);
-
-    let jokeData;
-
-    jokeDocument
-        .get()
-        .then((doc) => {
-            if (doc.exists) {
-                jokeData = doc.data();
-                jokeData.jokeId = doc.id;
-                return likeDocument.get();
-            } else {
-                return res.status(404).json({ error: 'joke not found' });
-            }
-        })
-        .then((data) => {
-            if (data.empty) {
-                return db
-                    .collection('likes')
-                    .add({
-                        jokeId: req.params.jokeId,
-                        userHandle: req.user.handle
-                    })
-                    .then(() => {
-                        jokeData.likeCount++;
-                        return jokeDocument.update({ likeCount: jokeData.likeCount });
-                    })
-                    .then(() => {
-                        return res.json(jokeData);
-                    });
-            } else {
-                return res.status(400).json({ error: 'joke already liked' });
-            }
-        })
-        .catch((err) => {
-            console.error(err);
-            res.status(500).json({ error: err.code });
-        });
+        // can only like a joke if the like document is EMPTY
+        if (likeDoc.empty) {
+            await db.collection('likes').add({
+                jokeId: req.params.jokeId,
+                userHandle: req.user.handle
+            });
+            // update jokeDocument
+            jokeDocument.update({ likeCount: jokeData.likeCount + 1 });
+            return res.json(jokeData);
+        } else {
+            return res.status(400).json({ error: 'joke already liked' });
+        }
+    } catch (err) {
+        return handleError(err, res);
+    }
 };
 
 
-const unlikeJoke = (req: any, res: any) => {
-    const likeDocument = db
-        .collection('likes')
-        .where('userHandle', '==', req.user.handle)
-        .where('jokeId', '==', req.params.jokeId)
-        .limit(1);
+const unlikeJoke = async (req: any, res: any) => {
+    try { // initialize documents
+        // this fetches firestore twice. Maybe change?
 
-    const jokeDocument = db.doc(`/Jokes/${req.params.jokeId}`);
+        const jokeDocument = db.doc(`/Jokes/${req.params.jokeId}`);
+        const jokeData: Joke = await getJoke(req.params.jokeId); // automatically checks if joke doc exists
+        const likeDoc = await getLikeDocument(req.user.handle, req.params.jokeId);
 
-    let jokeData;
+        if (!likeDoc.empty) {
+            // if there a joke is actually liked; i want to remove this 
+            await likeDoc.docs[0].ref.delete();
+            await jokeDocument.update({ likeCount: jokeData.likeCount - 1 });
+            return res.json(jokeData);
+        } else {
+            return res.status(400).json({ error: "joke already unliked" })
+        }
+    } catch (err) {
+        return handleError(err, res);
+    }
 
-    // pathway: get data from the joke document => if 
 
-    jokeDocument.get() // yes, always remember to check if a doc exists
-        .then((doc) => {
-            if (doc.exists) {
-                jokeData = doc.data();
-                jokeData.jokeId = doc.id;
-                return likeDocument.get();
-            } else {
-                return res.status(404).json({ error: 'joke not found' });
-            }
-        })
-        .then((doc) => {
-            if (!doc.empty) {
-                // if there a joke is actually liked; i want to remove this 
-                return doc.docs[0].ref.delete()
-                    .then(() => {
-                        jokeData.likeCount--;
-                        return jokeDocument.update({ likeCount: jokeData.likeCount });
-                    })
-                    .then(() => {
-                        return res.json(jokeData);
-                    });
-            } else {
-                return res.status(400).json({ error: "joke already unliked" })
-            }
-
-        })
-        .catch(err => {
-            console.error(err);
-            return res.json({ error: err.code });
-        });
 };
 
-const deleteJoke = (req: any, res: any) => {
-    let likeNumber;
-    //check if the joke was made by the user.
-    db.doc(`Jokes/${req.params.jokeId}`).get()
-        .then(doc => {
-            if (doc.exists) {
-                if (req.user.handle == doc.data().handle) {
-                    likeNumber = doc.data().likeCount;
-                    return doc.ref.delete();
-                } else {
-                    return res.status(400).json({ error: "Not Authorized" });
-                }
-            } else {
-                return res.status(500).json({ error: "Document not found" });
-            }
-        })
-        .then(() => {
-            return res.json({ delete: "Joke was not funny, so it was deleted" });
-        })
-        .catch(err => {
-            console.error(err);
-            return res.status(500).json({ error: err.code });
-        });
+const deleteJoke = async (req: any, res: any) => {
+    try {
+        // check if the joke was made by the user.
+        // since we need different errors if the joke does not exist, 
+        // we cannot use `await getJoke()`
+        const jokeDoc = await db.doc(`Jokes/${req.params.jokeId}`).get();
+        const joke: JokeNoID = jokeDoc.data() as JokeNoID;
 
-    // Deal with the user's like count LATER?? WIP
+        // TODO: use later
+        const likeCount = joke.likeCount;
+
+        if (jokeDoc.exists) {
+            // cheeck authorization
+            if (req.user.handle == joke.handle) {
+                await jokeDoc.ref.delete();
+            } else {
+                return res.status(400).json({ error: "Not Authorized" });
+            }
+        } else {
+            return res.status(500).json({ error: "Document not found" });
+        }
+
+        return res.json({ delete: "Joke was not funny, so it was deleted" });
+    } catch (err) {
+        return handleError(err, res);
+    }
+
+
+    // TODO: Deal with the user's like count LATER??
 };
 
 /*
-
+ 
     }
     const jokeId = req.params.jokeId.trim();
     const likeData = {
@@ -287,6 +258,68 @@ const deleteJoke = (req: any, res: any) => {
     })
 };
 */
+
+// HELPER FUNCTIONS
+
+// try catch is apparently nicer, but it seems like kind of an anti pattern
+// still very bloated. Im not sure if there's a better way though
+
+const getJoke = async (jokeId: string): Promise<Joke> => {
+    try {
+        const jokeDoc = await db.doc(`Jokes/${jokeId}`).get();
+        if (jokeDoc.exists) {
+            const joke: Joke = {
+                ...jokeDoc.data() as JokeNoID,
+                jokeId: jokeId
+            }
+            return joke;
+        } else {
+            throw new Error(error(404, `Joke with id ${jokeId} not found`));
+        }
+    } catch (err) {
+        throw new Error(error(500, err.code, `Unknown error when fetching joke with id ${jokeId}`));
+    }
+}
+
+// is a try/catch block the best way?
+const getJokeComments = async (jokeId: string): Promise<Comment[]> => {
+    try {
+        const querySnapshot = await db.collection('comments')
+            .orderBy('timeCreated', 'desc')
+            .where("jokeId", "==", jokeId)
+            .get()
+        let comments: Comment[] = [];
+        querySnapshot.forEach(doc => { comments.push(doc.data() as Comment) });
+        return comments;
+    }
+    catch (err) {
+        throw new Error(error(400, err.code, `Unknown error when fetching comments from joke with id ${jokeId}`))
+    }
+}
+
+
+const getLikeDocument = async (handle: string, jokeId: string): Promise<FirebaseFirestore.QuerySnapshot<FirebaseFirestore.DocumentData>> => {
+    return db
+        .collection('likes')
+        .where('userHandle', '==', handle)
+        .where('jokeId', '==', jokeId)
+        .limit(1)
+        .get();
+}
+
+
+// in a best case scenario, I would be able to know the type of 'res'
+// in actuality, I have no idea. any types are kind of a bad pracctice in typescript
+// bugs might occur if `err` doesn't have a `.code` attribute
+const handleError = async (err: any, res: any) => {
+    if (instanceOfError(err)) {
+        // we have access to status and msg
+        return res.status(err.status).json({ error: err.msg, extraInfo: err.extra })
+    } else {
+        // unknown msg
+        return res.status(500).json({ msg: `Unknown error: ${err.code}` })
+    }
+}
 
 export {
     getAllJokes,
--- sma-functions/functions/src/types/index.ts
+++ sma-functions/functions/src/types/index.ts
@@ -1,3 +1,7 @@
+export interface JokeWithComments extends JokeNoID {
+    comments: Comment[]
+}
+
 // bruh this is the same as sma-client joke
 export interface Joke extends JokeNoID {
     jokeId: string
@@ -21,3 +25,35 @@ export interface User {
     timeCreated: string,
     userId: string,
 }
+
+
+export interface Comment {
+    body: string,
+    jokeId: string,
+    timeCreated: string,
+    userHandle: string,
+    userImgUrl: string,
+}
+
+// used in jokes.ts where we have async functions that should hold status code when fails
+// to do things like res.status(code).json(code)
+// but in functions that don't have access to the status
+export interface Error {
+    status: number, // e.g. 404
+    msg: string,
+    extra?: string, // optional extra info
+}
+
+// check if an object is an error type
+export function instanceOfError(object: any): object is Error {
+    return ('status' in object) && ('msg' in object);
+}
+
+
+// promises CANNOT be strictly typed in the error side rip
+// https://github.com/microsoft/TypeScript/issues/6283
+// we have to unionize it with any 
+export type PError = Error | any
+
+// simple function call to make error. I've seen this in APIs before, but is it really good design?
+export const error = (status: number, msg: string, extra?: string): PError => ({ code: status, msg: msg, extra: extra })
