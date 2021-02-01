339087a98b9d28629152159f8d5c0a769682003c
joshuanianji <joshua.ji@jiwhiz.com>
2021-01-31 22:52:58 -0700

    Small edits to dbschema to make it typecheck

--- sma-functions/functions/src/dbschema.ts
+++ sma-functions/functions/src/dbschema.ts
@@ -8,45 +8,42 @@ let db = {
         userId: 'vc3u52asWKSJnTxaXnLS33j8NK32',
         imageUrl: 'https://firebasestorage.googleapis.com/v0/b/social-media-6297e.appspot.com/o/791.jpg?alt=media',
         website: 'https://google.com',
-        likes: [
-            joke1, 
-            joke2
-        ]
+        likes: []
     },
     jokes:
-        {
-            handle: 'user',
-            body: 'this is the content of the description',
-            timeCreated: '2020-12-29T00:35:29.543Z', // This is an "ISOString" or some shit.
-            imageUrl: "https://firebasestorage.googleapis.com/v0/b/social-media-6297e.appspot.com/o/791.jpg?alt=media",
-            likeCount: 5,
-            commentCount: 2,
-            jokeId: "nX07hphn1yd3OfDm7Uar", // try to reduce reads on our database since firebase charges based on reads.
-        },
+    {
+        handle: 'user',
+        body: 'this is the content of the description',
+        timeCreated: '2020-12-29T00:35:29.543Z', // This is an "ISOString" or some shit.
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/social-media-6297e.appspot.com/o/791.jpg?alt=media",
+        likeCount: 5,
+        commentCount: 2,
+        jokeId: "nX07hphn1yd3OfDm7Uar", // try to reduce reads on our database since firebase charges based on reads.
+    },
 }
 
 const userDetails = {
-        // redux data?? (its for our frontend applicaton and the information we use to populate the actual profile that the user sees)
-        credentials: {
-            handle: 'user',
-            email: 'someone@gmail.com',
-            timeCreated: '2021-01-03T20:46:26.852Z',
-            location: ' canada',
-            userId: 'vc3u52asWKSJnTxaXnLS33j8NK32',
-            imageUrl: 'https://firebasestorage.googleapis.com/v0/b/social-media-6297e.appspot.com/o/791.jpg?alt=media',
-            website: 'https://google.com',
-            },
-        likes: [
-            {
-                userHandle: 'user',
-                jokeId: "adlkfjaslekfj",
-            },
-            {
-                userHandle: 'user',
-                jokeId: "adlkfjaslekfj",
-            }
-        ]
-    };
+    // redux data?? (its for our frontend applicaton and the information we use to populate the actual profile that the user sees)
+    credentials: {
+        handle: 'user',
+        email: 'someone@gmail.com',
+        timeCreated: '2021-01-03T20:46:26.852Z',
+        location: ' canada',
+        userId: 'vc3u52asWKSJnTxaXnLS33j8NK32',
+        imageUrl: 'https://firebasestorage.googleapis.com/v0/b/social-media-6297e.appspot.com/o/791.jpg?alt=media',
+        website: 'https://google.com',
+    },
+    likes: [
+        {
+            userHandle: 'user',
+            jokeId: "adlkfjaslekfj",
+        },
+        {
+            userHandle: 'user',
+            jokeId: "adlkfjaslekfj",
+        }
+    ]
+};
 
 const comment = {
     body: "this is commenting on some joke",
@@ -63,10 +60,10 @@ const notifications = [
         jokeId: 'alksdjflaawehkfjha ;el',
         type: 'like | comment',
         createdAt: 'theISOSTRING DATE thing'
-}
+    }
 ]
 
 const likes = {
-    jokeId: "aldkjfaljef";
-    userHandle: "user";
+    jokeId: "aldkjfaljef",
+    userHandle: "user",
 }
